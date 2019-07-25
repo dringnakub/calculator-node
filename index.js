0bdb77314fb473846d3eed9b44b7a7869be6bb31
KKAWICHAI <dring_za@hotmail.com>
2019-07-25 12:04:19 +0700

    test divide success wait to branch master

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
     plus: (first, second) => first + second ,
     subtract: (first, second) => first - second,
     multipy: (first, second) => first * second,
-    divide: (first, second) => first / second,
+    divide: (first, second) => first / second, 
 }
