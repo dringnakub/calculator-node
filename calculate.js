7632a10631af0592476cf0d492485234f62196e3
KKAWICHAI <dring_za@hotmail.com>
2019-07-25 10:33:46 +0700

    add function multipy

--- calculate.js
+++ calculate.js
@@ -2,3 +2,4 @@ const cal = require('./index')
 
 console.log("plus 10 + 9 = " , cal.plus(10, 9))
 console.log("subtract 20 - 9 = ", cal.subtract(20, 9))
+console.log("multipy 9 x 9 = ", cal.multipy(9, 9))
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
     plus: (first, second) => first + second ,
     subtract: (first, second) => first - second,
+    multipy: (first, second) => first * second
 }
